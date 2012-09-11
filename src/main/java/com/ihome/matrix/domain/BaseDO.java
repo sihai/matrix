3b871ec9e9b9d84085a2269f9baa5cfb041efe99
sihai <sihai@iac-rqq.iac-rqq.com>
2012-09-12 00:30:24 +0800

    matrix

--- src/main/java/com/ihome/matrix/domain/BaseDO.java
+++ src/main/java/com/ihome/matrix/domain/BaseDO.java
@@ -1,7 +1,37 @@
 package com.ihome.matrix.domain;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 public class BaseDO implements Serializable {
-
+	
+	protected Long 	id;
+	protected Boolean isDeleted;
+	protected Date	gmtCreate;
+	protected Date	gmtModified;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	public Date getGmtCreate() {
+		return gmtCreate;
+	}
+	public void setGmtCreate(Date gmtCreate) {
+		this.gmtCreate = gmtCreate;
+	}
+	public Date getGmtModified() {
+		return gmtModified;
+	}
+	public void setGmtModified(Date gmtModified) {
+		this.gmtModified = gmtModified;
+	}
 }
